48a8e90e30c2eb6be2bda53759d783925873e14a
c.winger <c.winger@oeko.de>
2021-10-20 13:49:18 +0200

    formatting

--- datatools/storage/storage.py
+++ datatools/storage/storage.py
@@ -4,8 +4,10 @@ import tempfile
 import os
 import shutil
 
+
 class ObjectNotFoundException(Exception):
     pass
+
 
 class AbstractStorage:
     def get(self, file_id):
@@ -15,12 +17,12 @@ class AbstractStorage:
 
         Returns:
             data_stream (IOBase like)
-        
+
         Raises:
             ObjectNotFoundException
         """
         raise NotImplementedError()
-    
+
     def set(self, data_stream):
         """
         Args:
@@ -30,57 +32,56 @@ class AbstractStorage:
             file_id(str): 32 character md5 hash
         """
         raise NotImplementedError()
-       
+
 
 class HashedByteIterator:
-    DEFAULT_CHUNK_SIZE = 2 ** 1 # 2 ** 24
+    DEFAULT_CHUNK_SIZE = 2 ** 1  # 2 ** 24
 
     def __init__(self, data_stream):
         self.data_stream = data_stream
         self.hash = hashlib.md5()
         self.chunk_size = self.DEFAULT_CHUNK_SIZE
-    
+
     def __iter__(self):
         return self
 
     def __next__(self):
         chunk = self.data_stream.read(self.chunk_size)
-        logging.debug('read %d bytes (size=%d)', len(chunk), self.chunk_size)        
+        logging.debug("read %d bytes (size=%d)", len(chunk), self.chunk_size)
         if not chunk:
-            raise StopIteration()        
+            raise StopIteration()
         self.hash.update(chunk)
         return chunk
-
 
     def get_file_id(self):
         return self.hash.hexdigest()
 
 
 class FileSystemStorage(AbstractStorage):
-    
-    DEFAULT_DATA_DIR = '.cache'
-    
+
+    DEFAULT_DATA_DIR = ".cache"
+
     def __init__(self, data_dir=None):
         self.data_dir = os.path.abspath(data_dir or self.DEFAULT_DATA_DIR)
-        
+
         if not os.path.isdir(self.data_dir):
             logging.debug("creating data dir: %s", self.data_dir)
             os.makedirs(self.data_dir, exist_ok=True)
         else:
-            logging.debug("using data dir: %s", self.data_dir)        
+            logging.debug("using data dir: %s", self.data_dir)
 
     def get_filepath(self, file_id):
         return os.path.join(self.data_dir, file_id)
-        
+
     def get(self, file_id):
         filepath = self.get_filepath(file_id)
         if not os.path.isfile(filepath):
             raise ObjectNotFoundException(file_id)
-        return open(filepath, 'rb')
+        return open(filepath, "rb")
 
     def set(self, data_stream):
         data_stream = HashedByteIterator(data_stream)
-        with tempfile.TemporaryFile('wb', delete=False) as file:
+        with tempfile.TemporaryFile("wb", delete=False) as file:
             for chunk in data_stream:
                 file.write(chunk)
         file_id = data_stream.get_file_id()
@@ -88,10 +89,10 @@ class FileSystemStorage(AbstractStorage):
         tmp_filepath = file.name
         if os.path.isfile(filepath):
             # file exists already
-            logging.debug('file already in storage: %s', file_id)
+            logging.debug("file already in storage: %s", file_id)
             os.remove(tmp_filepath)
         else:
             # copy file
-            logging.debug('adding file: %s', filepath)
-            shutil.move(tmp_filepath, filepath)        
+            logging.debug("adding file: %s", filepath)
+            shutil.move(tmp_filepath, filepath)
         return file_id
--- test/test_storage.py
+++ test/test_storage.py
@@ -6,7 +6,10 @@ import os
 from datatools.storage.storage import FileSystemStorage, ObjectNotFoundException
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
-logging.basicConfig(format="[%(asctime)s %(levelname)7s] %(message)s", level=logging.DEBUG)
+logging.basicConfig(
+    format="[%(asctime)s %(levelname)7s] %(message)s", level=logging.DEBUG
+)
+
 
 class TestFileSystemStorage(unittest.TestCase):
     @classmethod
@@ -21,14 +24,14 @@ class TestFileSystemStorage(unittest.TestCase):
         pass
 
     def test_add_file(self):
-        file_name = '900150983cd24fb0d6963f7d28e17f72'        
-        
+        file_name = "900150983cd24fb0d6963f7d28e17f72"
+
         # try to load file that has not been added
         self.assertRaises(ObjectNotFoundException, lambda: self.storage.get(file_name))
 
         # add file and check if id matches name
-        filepath = os.path.join(TEST_DATA_DIR, file_name)        
-        with open(filepath, 'rb') as file:
+        filepath = os.path.join(TEST_DATA_DIR, file_name)
+        with open(filepath, "rb") as file:
             file_id = self.storage.set(file)
         self.assertEqual(file_id, file_name)
 
