425b922c24701f8823d224fd90f783c9a197e246
c.winger <c.winger@oeko.de>
2023-02-22 18:32:34 +0100

    refactoring, setup

---
+++ datatools/utils/__init__.py
@@ -0,0 +1,52 @@
+import datetime
+import getpass
+import hashlib
+import os
+import socket
+from stat import S_IREAD, S_IRGRP, S_IROTH
+
+import tzlocal
+
+DATETIMETZ_FMT = "%Y-%m-%d %H:%M:%S%z"
+DATE_FMT = "%Y-%m-%d"
+
+
+def make_readonly(filepath):
+    os.chmod(filepath, S_IREAD | S_IRGRP | S_IROTH)
+
+
+def get_hash(filepath, method="sha256"):
+    hasher = getattr(hashlib, method)()
+    with open(filepath, "rb") as file:
+        hasher.update(file.read())
+    result = {}
+    result[method] = hasher.hexdigest()
+    return result
+
+
+def get_now():
+    tz_local = tzlocal.get_localzone()
+    now = datetime.datetime.now()
+    now_tz = now.replace(tzinfo=tz_local)
+    return now_tz
+
+
+def get_now_str():
+    get_now().strftime(DATETIMETZ_FMT)
+
+
+def get_today_str():
+    get_now().strftime(DATE_FMT)
+
+
+def get_user_long():
+    def get_user():
+        """Return current user name"""
+        return getpass.getuser()
+
+    def get_host():
+        """Return current domain name"""
+        # return socket.gethostname()
+        return socket.getfqdn()
+
+    return f"{get_user()}@{get_host()}"
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+
+    setup(
+        packages=["datatools"],
+        keywords=[],
+        install_requires=[
+            "click",
+            "coloredlogs",
+            "appdirs",
+            "filelock",
+            "requests",
+            "tzlocal",
+        ],
+        name="datatools",
+        description=None,
+        long_description=None,
+        long_description_content_type="text/markdown",
+        version="0.0.0",
+        author="Christian Winger",
+        author_email="c@wingechr.de",
+        url="https://github.com/wingechr/datatools",
+        platforms=["any"],
+        license="Public Domain",
+        project_urls={"Bug Tracker": "https://github.com/wingechr/datatools"},
+        classifiers=[
+            "Programming Language :: Python :: 3.9",
+            "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+            "Operating System :: OS Independent",
+        ],
+        entry_points={"console_scripts": ["datatools = datatools.__main__:main"]},
+    )
