857df8e4297eb597beeba80b82bc96f8e921d795
c.winger <c.winger@oeko.de>
2023-07-24 13:18:46 +0200

    bumpversion

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         description=None,
         long_description=None,
         long_description_content_type="text/markdown",
-        version="0.0.0",
+        version="0.1.0",
         author="Christian Winger",
         author_email="c@wingechr.de",
         url="https://github.com/wingechr/datatools",
