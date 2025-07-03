2c56f47c662f135fc268562a90c2293f3d8218f3
c.winger <c.winger@oeko.de>
2025-07-03 07:55:47 +0200

    add list and create resource from data

--- datatools/utils/__init__.py
+++ datatools/utils/__init__.py
@@ -1,1 +1,8 @@
-import jsonpath_ng
+from functools import cache as _cache
+from functools import update_wrapper
+
+
+def cache(func):
+    """Update cache decorator that preserves metadata"""
+    cached_func = _cache(func)
+    return update_wrapper(cached_func, func)
--- tests/test_datatools_storage.py
+++ tests/test_datatools_storage.py
@@ -20,20 +20,45 @@ class TestTemplate(unittest.TestCase):
 
     def test_datatools_basics(self):
         storage = Storage(location=self.tempdir.name)
-        res = storage.resource("a/b.txt")
-        # read write metadata
-        value = "myvalue"
-        key = "mykey"
-        res.metadata.set(key, value)
+
+        # create resource
+        res = storage.get("a/b.txt")
+
+        # read/write metadata
+        key = "$mykey"
+        value = 999
+
+        res.metadata.set(**{key: value})
         self.assertEqual(res.metadata.get(key), value)
 
         # read write data
+        # data does not exist, even though we saved metadata
         self.assertFalse(res.exist())
+
         bdata = "test".encode()
         res.write(BytesIO(bdata))
         self.assertTrue(res.exist())
         with res.open() as file:
             self.assertTrue(file.read(), bdata)
 
-        res.delete()
+        # test list all resources and equality
+        self.assertTrue(res in storage.list())
+
+        # delete (but keep metadata)
+        res.delete(keep_metadata=True)
         self.assertFalse(res.exist())
+        # -> still in storage, because metadata still exists
+        self.assertTrue(res in storage.list())
+        res.delete()
+        self.assertFalse(res in storage.list())
+
+        # create again without having a path
+        res = storage.store(BytesIO(bdata), suffix=".txt")
+        self.assertEqual(res.path, "md5/098f6bcd4621d373cade4e832627b4f6.txt")
+        with res.open() as file:
+            self.assertTrue(file.read(), bdata)
+
+        # can get empty metadata without error, even if resource does not exist
+        self.assertEqual(
+            storage.get("nonexistent/path").metadata.get("nonexistent/key"), None
+        )
