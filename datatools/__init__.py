97aa6196204bac47936f7d3d1f3c3784addd443d
c.winger <c.winger@oeko.de>
2026-07-14 17:19:02 +0200

    version: 0.15.7 → 0.15.8

--- datatools/__init__.py
+++ datatools/__init__.py
@@ -1,6 +1,6 @@
 """data processing tools."""
 
-__version__ = "0.15.7"
+__version__ = "0.15.8"
 __all__ = ["FileDataStorage", "AnnotatedFunction"]
 
 from datatools.process.task import AnnotatedFunction
